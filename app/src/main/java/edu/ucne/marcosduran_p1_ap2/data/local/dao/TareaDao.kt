24c613c877245e9bfe7a7e3475810257617a2f16
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-06-04 18:53:03 -0400

    Creacion del Dao TareaDao

---
+++ app/src/main/java/edu/ucne/marcosduran_p1_ap2/data/local/dao/TareaDao.kt
@@ -0,0 +1,30 @@
+package edu.ucne.marcosduran_p1_ap2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.marcosduran_p1_ap2.data.local.entities.TareaEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SystemDao {
+    @Upsert()
+    suspend fun save(system: TareaEntity)
+    @Query(
+        """
+            SELECT *
+            FROM Tarea
+            WHERE tareaId=:id
+            LIMIT 1
+        """
+    )
+
+    suspend fun find (id: Int): TareaEntity?
+
+    @Delete
+    suspend fun  delete ()
+    @Query("SELECT * FROM tarea")
+
+    fun getAll(): Flow<List<TareaEntity>>
+}
