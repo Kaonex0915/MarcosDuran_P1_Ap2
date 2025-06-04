2506a308f82e4330ea8a330b5e73320b7e8f9f01
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-06-04 18:44:52 -0400

    Creacion de entidad TareaEntity

---
+++ app/src/main/java/edu/ucne/marcosduran_p1_ap2/data/local/entities/TareaEntity.kt
@@ -0,0 +1,13 @@
+package edu.ucne.marcosduran_p1_ap2.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "Tarea")
+data class TareaEntity(
+    @PrimaryKey
+    val tareaId: Int? = null,
+    val descripcion: String = "",
+    val tiempo: Int = 0
+)
