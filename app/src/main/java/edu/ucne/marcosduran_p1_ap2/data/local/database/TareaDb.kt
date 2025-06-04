4f8ba863c069d6559fd552c2f0bf810a79e6dd35
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-06-04 18:58:40 -0400

    Creacion de la base de datos

---
+++ app/src/main/java/edu/ucne/marcosduran_p1_ap2/data/local/database/TareaDb.kt
@@ -0,0 +1,17 @@
+package edu.ucne.marcosduran_p1_ap2.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.marcosduran_p1_ap2.data.local.dao.TareaDao
+import edu.ucne.marcosduran_p1_ap2.data.local.entities.TareaEntity
+
+@Database(
+    entities = [
+        TareaEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class TareaDb : RoomDatabase() {
+    abstract fun tareaDao(): TareaDao
+}
