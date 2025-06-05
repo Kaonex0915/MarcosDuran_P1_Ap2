18338f2ed64867b67d85c3621e79040048cc2e39
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-06-04 20:20:48 -0400

    creando el Viewmodel

--- app/src/main/java/edu/ucne/marcosduran_p1_ap2/presentation/tarea/TareaViewModel.kt
+++ app/src/main/java/edu/ucne/marcosduran_p1_ap2/presentation/tarea/TareaViewModel.kt
@@ -1,10 +1,14 @@
 package edu.ucne.marcosduran_p1_ap2.presentation.tarea
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.marcosduran_p1_ap2.data.local.entities.TareaEntity
 import edu.ucne.marcosduran_p1_ap2.data.repository.TareaRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -30,4 +34,112 @@ class SystemViewModel @Inject constructor(
             TareaEvent.New -> nuevo()
         }
     }
+    private fun save() {
+        viewModelScope.launch {
+            if (_uiState.value.descripcion.isNullOrBlank()){
+                _uiState.update {
+                    it.copy(errorMessage = "El campo esta vacio")
+                }
+                return@launch
+            }
+
+            try {
+                TareaRepository.save(_uiState.value.toEntity())
+
+                _uiState.update {
+                    it.copy(errorMessage = null)
+                }
+            }catch (e: Exception){
+                _uiState.update {
+                    it.copy(errorMessage = "Error al guardar: ${e.message}")
+
+                }
+            }
+        }
+    }
+
+    private fun nuevo() {
+        _uiState.update {
+            it.copy(
+                tareaId = null,
+                descripcion = "",
+                tiempo = 0,
+                errorMessage = null
+            )
+        }
+    }
+
+    fun selectedTara(tareaId: Int){
+        viewModelScope.launch {
+            if(tareaId > 0){
+                try {
+                    val tarea = TareaRepository.getTarea(tareaId)
+                    _uiState.update {
+                        it.copy(
+                            tareaId = tarea?.tareaId,
+                            descripcion = tarea?.descripcion ?: "",
+                            tiempo = tarea?.tiempo,
+                            errorMessage = null
+                        )
+                    }
+                } catch (e: Exception) {
+                    _uiState.update {
+                        it.copy(errorMessage = "Error al cargar sistema: ${e.message}")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun delete() {
+        viewModelScope.launch {
+            try {
+                TareaRepository.delete(_uiState.value.toEntity())
+            } catch (e: Exception) {
+                _uiState.update {
+                    it.copy(errorMessage = "Error al eliminar: ${e.message}")
+                }
+            }
+        }
+    }
+
+    private fun getTareas() {
+        viewModelScope.launch {
+            TareaRepository.getTareas().collect { tareas ->
+                _uiState.update {
+                    it.copy(tareas = tareas)
+                }
+            }
+        }
+    }
+
+    private fun onDecripcionChange(descripcion: String) {
+        _uiState.update {
+            it.copy(
+                descripcion = descripcion,
+                errorMessage = null
+            )
+        }
+    }
+    private fun onTiempoChange(tiempo: Int) {
+        _uiState.update {
+            it.copy(
+                tiempo = tiempo,
+                errorMessage = null
+            )
+        }
+    }
+
+    private fun onTareaIdChange(tareaId: Int) {
+        _uiState.update {
+            it.copy(tareaId = tareaId)
+        }
+    }
+
+
 }
+fun TareaUiState.toEntity() = TareaEntity(
+    tareaId = tareaId,
+    descripcion = descripcion ?: "",
+    tiempo = tiempo,
+)
