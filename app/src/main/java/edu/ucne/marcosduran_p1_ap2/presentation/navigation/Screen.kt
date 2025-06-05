e98502bb2b69f0fad0da81bc3b261007dc8d74eb
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-06-04 20:42:57 -0400

    Creando Screen

---
+++ app/src/main/java/edu/ucne/marcosduran_p1_ap2/presentation/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package edu.ucne.marcosduran_p1_ap2.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Screen {
+    @Serializable
+    data object TareaList : Screen()
+    @Serializable
+    data class Tarea(val tareaId: Int) : Screen()
+}
