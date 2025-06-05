11257ce094cc6bd9acf3ae9ee050b5ee5fdc04e6
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-06-04 20:37:57 -0400

    Modificando Main actitivity

--- app/src/main/java/edu/ucne/marcosduran_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/marcosduran_p1_ap2/MainActivity.kt
@@ -4,45 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.marcosduran_p1_ap2.presentation.navigation.TareaNavHost
 import edu.ucne.marcosduran_p1_ap2.ui.theme.MarcosDuran_P1_Ap2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MarcosDuran_P1_Ap2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navHost = rememberNavController()
+                TareaNavHost(navHost)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MarcosDuran_P1_Ap2Theme {
-        Greeting("Android")
-    }
-}
 //tarea id, descripcion, tiempo
